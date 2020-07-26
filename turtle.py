5f3c9a15e9baa62b72681129253722531bfc05a1
Sahil4UI <54834362+Sahil4UI@users.noreply.github.com>
2020-07-26 13:59:32 +0530

    Add files via upload

---
+++ turtle.py
@@ -0,0 +1,20 @@
+Python 3.8.3 (v3.8.3:6f8c8320e9, May 13 2020, 16:29:34) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import turtle
+>>> turtle.Screen()
+<turtle._Screen object at 0x7fb4ecf0f430>
+>>> turtle.pen()
+{'shown': True, 'pendown': True, 'pencolor': 'black', 'fillcolor': 'black', 'pensize': 1, 'speed': 3, 'resizemode': 'noresize', 'stretchfactor': (1.0, 1.0), 'shearfactor': 0.0, 'outline': 1, 'tilt': 0.0}
+>>> turtle.shape('turtle')
+>>> turtle.forward(100)
+>>> turtle.left(60)
+>>> turtle.left(30)
+>>> turtle.left(-60)
+>>> turtle.reset()
+>>> turtle.forward(200)
+>>> turtle.left(120)
+>>> turtle.forward(200)
+>>> turtle.left(120)
+>>> turtle.forward(200)
+>>> 
